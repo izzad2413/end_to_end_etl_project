2af2dc197dbbd7d84c1f49d434da2a300b5cda4e
NAZMIRUL IZZAD BIN NASIR <nazmirul.izzad@city.edu.my>
2025-09-08 17:54:09 +0800

    init etl pipeline

---
+++ src/etl_pipeline.py
@@ -0,0 +1,8 @@
+import os
+import pandas as pd
+import sqlite3
+
+# THE PATHS
+RAW_DATA_PATH = './../data/raw/environment_kl.csv'
+PROCESSED_DATA_PATH = './../data/processed/environment_kl_cleaned.csv'
+DB_PATH = './../data/processed/environment_kl.db'
